eab2f13fd5f2e0f68510ce1a95da8895b382c6b7
Shaun Burdick <github@shaunburdick.com>
2024-08-31 23:35:54 -0400

    Added a logo!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hd-homey
+# HD Homey
+
 A Proxy App for HD Homerun devices
+
+![HD Homey](public/HD-Homey.webp)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main>
             <h1>Hello</h1>
+            <img src="HD-Homey.webp" alt="HD Homey" />
         </main>
     );
 }
