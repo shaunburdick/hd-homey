e2daaee50e84433bf65dc7bcfd4e292fe6835102
Shaun Burdick <github@shaunburdick.com>
2024-09-02 16:55:39 -0400

    Fix main image url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <main>
-            <img src="HD-Homey.webp" alt="HD Homey" style={{ width: '30vw' }} />
+            <img src="hd-homey.webp" alt="HD Homey" style={{ width: '30vw' }} />
             <h1>Hello</h1>
 
             <ul>
