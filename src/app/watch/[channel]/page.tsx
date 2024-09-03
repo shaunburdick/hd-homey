5c53fb9c0236d2677b34fa19e6b0bc0ffc0c0f4c
Shaun Burdick <github@shaunburdick.com>
2024-09-02 20:54:41 -0400

    Make the watch page a little easier to use

--- src/app/watch/[channel]/page.tsx
+++ src/app/watch/[channel]/page.tsx
@@ -1,10 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import Logger from '@/lib/logger';
+
 export default function Page({ params }: { params: { channel: string } }) {
+
+    const channelLink = `${location.origin}/stream/${params.channel}`;
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleCopy = async () => {
+        try {
+            Logger.debug(`Copying ${channelLink} to clipboard...`);
+            await navigator.clipboard.writeText(channelLink);
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 2000);
+        } catch (err) {
+            Logger.error(err);
+        }
+    };
 
     return (
         <main>
             <h1>Channel {params.channel}</h1>
             <p>
-                Copy this <a href={`/stream/${params.channel}`}>Link</a> into your favorite media player.
+                Copy this <a href={`/stream/${params.channel}`}>Link</a> into your favorite media player.<br />
+                <input
+                    type='text'
+                    value={channelLink}
+                    onFocus={(e) => e.target.select()}
+                    readOnly
+                    style={{
+                        width: `${channelLink.length}em`
+                    }}
+                />
+                <button onClick={handleCopy}>{isCopied ? 'Copied!' : 'Copy'}</button>
             </p>
             <p>
                 For example, in <a href="https://www.videolan.org/vlc/">VLC</a>:
