7931d4cea5e8bf4dee673a83bbf3796fb9fb07cc
Shaun Burdick <github@shaunburdick.com>
2024-09-15 14:19:42 -0400

    Add session name to greeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import hdHomey from '@public/hd-homey.webp';
+import { auth } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth();
     return (
         <>
             <Image src={hdHomey} alt="HD Homey" width={375} height={375} priority/>
-            <h1>Hello</h1>
+            <h1>Hello {session?.user.name}!</h1>
         </>
     );
 }
